69726cf98bfa5525394ab1c991329d35a8c690a8
Chand Anand <canand@uber.com>
2024-03-19 14:10:26 -0700

    add template filepath read logic

--- software-engineer/gen.go
+++ software-engineer/gen.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v2"
 )
 
+const (
+	_dataFile    = "data.yml"
+	_templateDir = "tmpl"
+)
+
 func get_resume_data() (Resume, error) {
-	data_file, err := os.ReadFile("data.yml")
+	data_file, err := os.ReadFile(_dataFile)
 	if err != nil {
 		return Resume{}, err
 	}
@@ -22,6 +28,26 @@ func get_resume_data() (Resume, error) {
 	return resume_data, nil
 }
 
+func get_tmpl_filepaths(dirName string) ([]string, error) {
+	var filepaths []string
+
+	if err := filepath.Walk(dirName,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !info.IsDir() {
+				filepaths = append(filepaths, path)
+			}
+			return nil
+		}); err != nil {
+		return nil, err
+	}
+
+	return filepaths, nil
+}
+
 func main() {
 	resume_data, err := get_resume_data()
 	if err != nil {
@@ -29,4 +55,11 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", resume_data)
+
+	filepaths, err := get_tmpl_filepaths(_templateDir)
+	if err != nil {
+		log.Fatal("error reading tmpl dir: ", err)
+	}
+
+	fmt.Println(filepaths)
 }
